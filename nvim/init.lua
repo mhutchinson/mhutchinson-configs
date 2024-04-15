a4424bf6bb29847f6ea0d93029ebb643ab5b78cf
Martin Hutchinson <mhutchinson@gmail.com>
2024-04-15 14:27:20 +0000

    Add go tooling

--- nvim/init.lua
+++ nvim/init.lua
@@ -228,6 +228,20 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require("lazy").setup({
 	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+	},
+	{
 		"romgrk/barbar.nvim",
 		dependencies = {
 			"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
