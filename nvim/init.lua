4aaf55100fd77e7eceae5fc126a6644400d57a5f
Martin Hutchinson <mhutchinson@gmail.com>
2024-04-15 20:03:01 +0100

    Markdown preview
    This runs live, which is neat

--- nvim/init.lua
+++ nvim/init.lua
@@ -261,6 +261,14 @@ require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 	"tpope/vim-unimpaired",
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
