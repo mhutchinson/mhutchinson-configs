c9655e34829f8eb13e53aa130f309fffd5a342df
Martin Hutchinson <mhutchinson@gmail.com>
2024-04-23 16:30:26 +0100

    More scrolloff and smaller tabstops

--- nvim/init.lua
+++ nvim/init.lua
@@ -154,7 +154,9 @@ vim.opt.inccommand = "split"
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 20
+
+vim.o.tabstop = 4
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
