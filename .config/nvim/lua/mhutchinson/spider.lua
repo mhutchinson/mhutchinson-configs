afe1dbe40f0e97fdaa957184d339da155240fa48
Martin Hutchinson <mhutchinson@gmail.com>
2024-05-16 08:53:36 +0000

    Improvements

--- .config/nvim/lua/mhutchinson/spider.lua
+++ .config/nvim/lua/mhutchinson/spider.lua
@@ -2,9 +2,24 @@ return {
 	{
 		"chrisgrieser/nvim-spider",
 		config = function()
-			vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
-			vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
-			vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
+			vim.keymap.set(
+				{ "n", "o", "x" },
+				"w",
+				"<cmd>lua require('spider').motion('w', { skipInsignificantPunctuation = false })<CR>",
+				{ desc = "Spider-w" }
+			)
+			vim.keymap.set(
+				{ "n", "o", "x" },
+				"e",
+				"<cmd>lua require('spider').motion('e', { skipInsignificantPunctuation = false })<CR>",
+				{ desc = "Spider-e" }
+			)
+			vim.keymap.set(
+				{ "n", "o", "x" },
+				"b",
+				"<cmd>lua require('spider').motion('b', { skipInsignificantPunctuation = false })<CR>",
+				{ desc = "Spider-b" }
+			)
 		end,
 	},
 }
--- .config/nvim/lua/mhutchinson/tabs.lua
+++ .config/nvim/lua/mhutchinson/tabs.lua
@@ -5,8 +5,14 @@ return {
 			"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
 			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
 		},
+		-- config = function()
+		-- end,
 		init = function()
 			vim.g.barbar_auto_setup = false
+			vim.keymap.set("n", "<leader>bc", "<cmd>BufferClose<CR>", { desc = "Close the current buffer" })
+			vim.keymap.set("n", "<leader>bn", "<cmd>BufferNext<CR>", { desc = "Go to the Next buffer" })
+			vim.keymap.set("n", "<leader>bp", "<cmd>BufferPrevious<CR>", { desc = "Go to the Previous buffer" })
+			vim.keymap.set("n", "<leader>bm", "<cmd>BufferPick<CR>", { desc = "Open the Magic buffer picker" })
 		end,
 		opts = {
 			-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
